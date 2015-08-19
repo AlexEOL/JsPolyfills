5e57654383ec317c212e68a4ce5e4c0edde31d8d
AlexEOL <alexander.melnik90@yandex.ru>
2015-08-19 18:26:11 +0300

    add throw error in filter, forEach

--- array/forEach.js
+++ array/forEach.js
@@ -1,12 +1,22 @@
 /**
  * forEach polify
  *
+ * @method     forEach
  * @param      {Function}  callback  (description)
  * @param      {Object}    context   (description)
- *
  * @return     {undefined}  (description_of_the_return_value)
  */
 function forEach(callback, context) {
+  "use strict";
+
+  if (this === null || this === undefined) {
+    throw new TypeError("Array.prototype.forEach called on null or undefined");
+  };
+
+  if (typeof callback !== "function") {
+    throw new TypeError(callback + " is not a function");
+  };
+
   var length = this.length,
       i      = 0;
 
