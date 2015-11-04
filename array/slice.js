6f1fb576620a05235362bcc616a30f0b0744ce83
Alexander Melnik <Alexander.Melnik90@Yandex.ru>
2015-11-04 16:55:55 +0300

    fix bug in slice method

--- array/slice.js
+++ array/slice.js
@@ -6,7 +6,11 @@
  *
  * @return     {Array}   (description_of_the_return_value)
  */
-function slice(begin, end) {
+function slice( begin, end ) {
+  if (arguments.length === 1) {
+    end = this.length;
+  }
+
   var returnArr = [],
       i         = ~~begin >= 0 ? ~~begin : this.length+~~begin,
       length    = (!!end || ~~end === 0) ? (~~end >= 0 ? ~~end : this.length+~~end) : this.length;
